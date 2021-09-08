76c55872098cb2567121476f434bda057ebe863a
eovip <eovip@192.168.1.5>
2021-09-08 13:51:51 +0700

    Bai 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello the World");
+		System.out.println("Hello World");
 	}
 
 }
